2cdb1ab298c44b36027a7142a8e29377fb36b21b
Ernesto  Catumbela <157809193+Catumbela12@users.noreply.github.com>
2026-05-10 18:47:10 +0100

    Update router.php

--- neucat_store/router.php
+++ neucat_store/router.php
@@ -4,5 +4,5 @@ if (preg_match('/\.(?:png|jpg|jpeg|gif|webp|css|js)$/', $_SERVER["REQUEST_URI"])
     return false; // serve o arquivo estático diretamente
 } else {
     $_GET['url'] = ltrim($_SERVER["REQUEST_URI"], '/');
-    require_once 'public/index.php';
+    require_once __DIR__ . '/public/index.php';
 }
