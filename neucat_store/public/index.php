29405445d274a7de96fe4dba16fe56a86c384de4
Ernesto  Catumbela <157809193+Catumbela12@users.noreply.github.com>
2026-05-10 16:16:34 +0100

    Refactor index.php to use __DIR__ for bootstrap

--- neucat_store/public/index.php
+++ neucat_store/public/index.php
@@ -1,3 +1,9 @@
+
 <?php
-require_once '../app/bootstrap.php';
+// public/index.php
+
+// Carrega configurações + bootstrap
+require_once __DIR__ . '/../app/bootstrap.php';
+
+// Inicia o Core (MVC)
 $init = new Core();
